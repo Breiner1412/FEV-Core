c4be5020a4965c0e610df120703944c2eeccf113
Breiner1412 <rodriguezbreiner125@gmail.com>
2026-09-25 16:16:08 -0500

    feat: crear solucion con los proyectos base (H0)

---
+++ tests/FevCore.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace FevCore.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/FevCore.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace FevCore.Integration.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
